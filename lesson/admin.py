6d67948446fe49c0b3ec5e5a6201b7572f9a1617
Lazizbek <102922781+MLA2317@users.noreply.github.com>
2023-07-28 15:51:02 +0500

    School Table Done

--- lesson/admin.py
+++ lesson/admin.py
@@ -4,4 +4,8 @@ from .models import Subject, LessonRoom, Lesson, Teacher
 admin.site.register(Subject),
 admin.site.register(LessonRoom),
 admin.site.register(Lesson),
-admin.site.register(Teacher)
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'subject_taught', 'level', 'teacher_img', 'created_date')
--- lesson/views.py
+++ lesson/views.py
@@ -27,6 +27,7 @@ class LessonRoomRUD(generics.RetrieveUpdateDestroyAPIView):
     queryset = LessonRoom.objects.all()
     serializer_class = LessonRomSerializer
     lookup_field = 'pk'
+    permission_classes = [permissions.IsAdminUser]
 
 
 class TeacherListCreate(generics.ListCreateAPIView):
@@ -50,72 +51,3 @@ class LessonRUD(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = LessonSerializer
     lookup_field = 'pk'
 
-
-
-
-
-
-
-
-
-
-
-
-# class LessonListCreate(APIView):
-#
-#     def get(self, request, format=None):
-#         lessons = Lesson.objects.all()
-#         serializer = LessonSerializer(lessons, many=True)
-#         return Response(serializer.data)
-#
-#     def post(self, request):
-#         author = self.request.user
-#         subject_id = self.request.data.get('subject_id')
-#         lesson_num = self.request.data.get('lesson_num')
-#         teacher_id = self.request.data.get('teacher_id')
-#         duration = self.request.data.get('duration')
-#         time = self.request.data.get('time')
-#
-#         subject = get_object_or_404(Subject, id=subject_id)
-#         lesson_room = get_object_or_404(LessonRoom, id=lesson_num)
-#         teacher = get_object_or_404(Teacher, id=teacher_id)
-#
-#         lesson = Lesson.objects.create(
-#             author=author,
-#             subject_id=subject,
-#             lesson_num=lesson_room,
-#             teach_id=teacher,
-#             duration=duration,
-#             time=time
-#         )
-#         serializer = LessonSerializer(lesson)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#
-# class LessonDetail(APIView):
-#     def get_object(self, pk):
-#         try:
-#             return Lesson.objects.get(pk=pk)
-#         except Lesson.DoesNotExist:
-#             raise Http404
-#
-#     def get(self, request, pk, format=None):
-#         lesson = self.get_object(pk)
-#         serializer = LessonSerializer(lesson)
-#         return Response(serializer.data)
-#
-#     def put(self, request, pk, format=None):
-#         lesson = self.get_object(pk)
-#         serializer = LessonSerializer(lesson, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     def delete(self, request, pk, format=None):
-#         lesson = self.get_object(pk)
-#         lesson.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
